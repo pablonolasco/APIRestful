565c72679dcfe660e5efa123780c4e1f92983cb9
Pablo <pablo.nolasco@live.com.mx>
2021-09-29 07:53:31 -0500

    se agregaron las operaciones para buyer

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Controllers\Buyer;
 
+use App\Buyer;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class BuyerController extends Controller
 {
-    //
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     *  TODO lista de usuarios
+     */
+    public function index()
+    {
+        // obtiene la funcion con la relacion del modelo transactions
+        $compradores=Buyer::has('transactions')->get();
+        return response()->json(['data'=>$compradores],200);
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        $comprador=Buyer::has('transactions')->findOrFail($id);
+        return response()->json(['data'=>$comprador],200);
+
+    }
 }
